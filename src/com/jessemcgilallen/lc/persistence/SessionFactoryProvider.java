e965f15c6b2f3c2f9943274e209b7b545d65ce07
Jesse McGil Allen <jesse@jessemcgilallen.com>
2016-02-09 19:16:53 -0600

    adding a SessionFactoryProvider

---
+++ src/com/jessemcgilallen/lc/persistence/SessionFactoryProvider.java
@@ -0,0 +1,35 @@
+package com.jessemcgilallen.lc.persistence;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+/**
+ * This file provides a SessionFactory for DAOs using Hibernate.
+ * Using paulawaite's file as a template
+ * @author jessemcgilallen
+ * @version 1.0 Febraury 9th, 2016
+ *
+ */
+
+public class SessionFactoryProvider {
+
+    private static SessionFactory sessionFactory;
+
+    public static void createSessionFactory() {
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(
+                configuration.getProperties()). buildServiceRegistry();
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+    }
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            createSessionFactory();
+        }
+        return sessionFactory;
+
+    }
+}
