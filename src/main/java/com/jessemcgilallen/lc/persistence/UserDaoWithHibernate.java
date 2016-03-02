d7ce2694026b105c8a46059700174a7ecc4b172b
Jesse McGil Allen <jesse@jessemcgilallen.com>
2016-03-01 23:44:54 -0600

    adding gradle one more time

--- src/main/java/com/jessemcgilallen/lc/persistence/UserDaoWithHibernate.java
+++ src/main/java/com/jessemcgilallen/lc/persistence/UserDaoWithHibernate.java
@@ -2,7 +2,9 @@ package com.jessemcgilallen.lc.persistence;
 
 import com.jessemcgilallen.lc.entity.User;
 
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -18,7 +20,7 @@ import java.util.List;
  */
 public class UserDaoWithHibernate implements UserDao {
 
-    private final Logger log = Logger.getLogger(this.getClass());
+    private final Logger log = LogManager.getLogger(this.getClass());
 
     /**
      * @param user to be added
